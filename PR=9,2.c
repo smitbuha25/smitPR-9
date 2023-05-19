433b701fdc0354789a1570b5bbe6756c421f6c49
smitbuha25 <130464705+smitbuha25@users.noreply.github.com>
2023-05-19 21:20:17 +0530

    Add files via upload

---
+++ PR=9,2.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+ 
+enum months 
+{
+	Sunday,
+	Monday,
+	Tuseday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+};  
+int main()  
+{ 
+  int a;
+  int n,i; 
+  for(i=Sunday;i<=Saturday;i++)  
+ 
+    {  
+        printf("%d\n",i);  
+    }  
+}  
